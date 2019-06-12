d9e9d3576a5770b38bdba03c49987c944ab1eac8
malachma <malachma@microsoft.com>
2019-06-12 12:24:36 +0200

    mount logic changed to a for-loop

--- fstab.sh
+++ fstab.sh
@@ -34,29 +34,26 @@ mount -o nouuid $boot_part /mnt/rescue-boot
 #Mount the support filesystems
 #==============================
 #see also http://linuxonazure.azurewebsites.net/linux-recovery-using-chroot-steps-to-recover-vms-that-are-not-accessible/
-cd /mnt/rescue-root
-mount -t proc proc proc
-mount -t sysfs sys sys/
-mount -o bind /dev dev/
-mount -o bind /dev/pts dev/pts/
 
-chroot /mnt/rescue-root
+for i in dev proc sys dev/pts
+do
+	mount -o bind /$i /mnt/rescue-root/$i
+done
 
+
+cat EOF << | chroot /mnt/rescue-root
 mv /etc/fstab{,.org}
 cat /etc/fstab.org | awk '/\/ /{print}' >> /etc/fstab
 cat /etc/fstab.org | awk '/\/boot /{print}' >> /etc/fstab
 cat /etc/fstab
-
-
 exit
-cd /
-umount /mnt/rescue-root/proc
-umount /mnt/rescue-rootsys
-umount /mnt/rescue-root/dev/pts
-umount /mnt/rescue-root/dev
-umount /mnt/rescue-boot
-umount /mnt/rescue-root
 
+
+
+for i in dev/pts proc sys dev
+do
+	umount  /mnt/rescue-root/$i
+done
 
 
 
