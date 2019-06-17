25c9a28c584e04498b03fc02d3dd13285581e5a3
malachma <malachma@microsoft.com>
2019-06-17 16:50:15 +0200

    remove files

--- fstab.sh
+++ fstab.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# What is the Distro type?
+#distro_name=$(cat /etc/os-release | awk "/^NAME/" | sed -e 's/NAME=//')
+#distro_name=${distro_name//\"}
 
-#Discover the stucture of the os-data-disk
-#==========================================
+
+# Variables
+amount_of_partitions=0 
+UBUNTU_DISTRO="true"
 
 #get boot flaged partition
 #-------------------------
@@ -11,9 +16,23 @@ boot_part=$(fdisk -l /dev/sdc | awk '$2 ~ /\*/ {print $1}')
 #---------------------
 partitions=$(fdisk -l /dev/sdc | awk '/^\/dev\/sdc/ {print $1}')
 
-#get the root partition
-#-----------------------
-rescue_root=$(echo $partitions | sed "s|$boot_part||g")
+for i in $partitions; do 
+    ((amount_of_partitions++));
+done
+
+# Determine what distro we have to recover
+if [[ $amount_of_partitions -gt 1 ]]; 
+then
+    # This is a RedHat baased OS-Disk
+    rescue_root=$(echo $partitions | sed "s|$boot_part||g")
+    UBUNTU_DISTRO="false"
+
+else
+    # This is an Ubuntu/Debian based OS-Disk
+    rescue_root=$boot_part
+    UBUNTU_DISTRO="true"
+fi
+
 
 
 
@@ -21,13 +40,19 @@ rescue_root=$(echo $partitions | sed "s|$boot_part||g")
 #====================
 
 mkdir /mnt/rescue-root
-mount -o nouuid $rescue_root /mnt/rescue-root
+if [[ $UBUNTU_DISTRO == "false" ]];
+then
+    mount -o nouuid $rescue_root /mnt/rescue-root
+else
+    mount $rescue_root /mnt/rescue-root
+fi
 
 #Mount the boot part
 #===================
-
-mkdir /mnt/rescue-boot
-mount -o nouuid $boot_part /mnt/rescue-boot
+if [[ $UBUNTU_DISTRO == "false" ]];
+then
+    mount -o nouuid $boot_part /mnt/rescue-root/boot
+fi
 
 
 
@@ -36,11 +61,11 @@ mount -o nouuid $boot_part /mnt/rescue-boot
 #see also http://linuxonazure.azurewebsites.net/linux-recovery-using-chroot-steps-to-recover-vms-that-are-not-accessible/
 
 for i in dev proc sys dev/pts; do mount -o bind /$i /mnt/rescue-root/$i; done
+if [[ $UBUNTU_DISTRO == "true" ]];
+then
+    mount -o bind /run /mnt/rescue-root/run
+fi
 
-
-#################################
-# Fix the fstab to allow a boot #
-#################################
 chroot /mnt/rescue-root << EOF
 mv -f /etc/fstab{,.copy}
 cat /etc/fstab.copy | awk '/\/ /{print}' >> /etc/fstab
@@ -51,11 +76,16 @@ EOF
 
 
 #Clean up everything
+cd /
 for i in dev/pts proc sys dev; do umount  /mnt/rescue-root/$i; done
 
-umount /mnt/rescue-boot
+if [[ $UBUNTU_DISTRO == "true" ]];
+then
+    umount /mnt/rescue-root/run
+fi
+umount /mnt/rescue-root/boot
 umount /mnt/rescue-root                                                                                                                                                                              
-rm -fr /mnt/rescue-*
+rm -fr /mnt/rescue-root
 
 
 
